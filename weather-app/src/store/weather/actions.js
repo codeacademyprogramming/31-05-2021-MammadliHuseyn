64b09875e62a303ccc20ed11370c854f74550d11
MammadliHuseyn <Huseynym@code.edu.az>
2021-06-01 03:53:47 +0400

    Unit test for weather component

--- weather-app/src/store/weather/actions.js
+++ weather-app/src/store/weather/actions.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { ACTION_TYPES } from './../actionTypes';
+const apiKey = "af6e0a6c0cb014eff6a9dc473b056967";
 
 export const getWeatherByCity = (city) => {
     return (dispatch) => {
-        return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=16596fe956171a7376f2ba91213e3499`)
+        return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
             .then(
                 ({ data }) => dispatch({ type: ACTION_TYPES.GET_WEATHER_BY_CITY, payload: data }),
                 err => console.log(err)
@@ -13,7 +14,7 @@ export const getWeatherByCity = (city) => {
 
 export const deleteWeatherbyCity = (id) => {
     return {
-        type:ACTION_TYPES.DELETE_WEATHER_BY_CITY,
-        payload:id
+        type: ACTION_TYPES.DELETE_WEATHER_BY_CITY,
+        payload: id
     }
 }
